26bc3f4d41ea4f9858a81b9a5d69858e3ae7a04d
phopper-pivotal <phopper@pivotal.io>
2017-08-16 07:56:22 -0600

    add config-repo directory

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SpringCloudConfig
+
+## Create Config Service Instance
+
+```
+'{"git": { "uri": "https://github.com/phopper-pivotal/SpringCloudConfig/config-repo", "label": "develop" } }'
+```
--- src/main/java/io/pivotal/SpringCloudConfig/SpringCloudConfigApplication.java
+++ src/main/java/io/pivotal/SpringCloudConfig/SpringCloudConfigApplication.java
@@ -1,10 +1,26 @@
 package io.pivotal.SpringCloudConfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@RestController
 public class SpringCloudConfigApplication {
+
+	@Value("${config.hello}")
+	String hello = "Hello";
+
+	@RequestMapping("/")
+	public String home() {
+		return hello + " World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigApplication.class, args);
